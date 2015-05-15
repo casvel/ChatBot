482fc1c42a678bf245f3b6f923d98809a583dd37
casvel <casvel.d@gmail.com>
2015-05-14 20:55:43 -0500

    conexion DB

--- src/ChatBotController.java
+++ src/ChatBotController.java
@@ -1,4 +1,38 @@
+import java.sql.*;
 
-public class ChatBotController {
-
+public class ChatBotController 
+{
+	static Connection con;
+	
+	static void Conecta()
+	{
+		String host = "jdbc:mysql://localhost:3306/Empresa";
+		String user = "admin";
+		String pass = "guapa";
+		
+		try
+		{
+			con = DriverManager.getConnection(host, user, pass);
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	static void Prueba()
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM puesto");
+			while (rs.next())
+				System.out.println(rs.getString("nombre"));
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}
+	}
+	
 }
--- src/Vicky.java
+++ src/Vicky.java
@@ -1,4 +1,11 @@
 
+
 public class Vicky {
 
+	public static void main(String[] args)
+	{
+		ChatBotController.Conecta();
+		ChatBotController.Prueba();
+	}
+	
 }
